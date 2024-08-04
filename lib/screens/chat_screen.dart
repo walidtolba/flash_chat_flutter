e64b4cd96cbb7ceda757461a32da764926043382
Walid Tolba <wrt0624@gmail.com>
2024-08-04 19:11:30 +0100

    fixed UI

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,34 +2,37 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/constants.dart';
+import 'package:flutter/services.dart';
+
+FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  late User loggedInUser;
+
 
 class ChatScreen extends StatefulWidget {
-    static const id = 'chat_screen';
+  static const id = 'chat_screen';
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
+  
 }
 
 class _ChatScreenState extends State<ChatScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  late User loggedInUser;
-  String message = '';
+  TextEditingController controller = TextEditingController();
 
-  void getCurrentUser()async{
-    try{
+  void getCurrentUser() async {
+    try {
       final user = await _auth.currentUser;
-          if (user != null)
-          {
-            loggedInUser = user;
-            print(loggedInUser);
-          }
-    }catch (e){
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser);
+      }
+    } catch (e) {
       print(e);
     }
   }
 
-@override
+  @override
   void initState() {
     getCurrentUser();
     super.initState();
@@ -56,25 +59,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder(stream: _firestore.collection('messages').snapshots(), builder: (context, snapshot) {
-              if (snapshot.hasData){
-                final messages = snapshot.data!.docs;
-                List<Widget> widgets = [];
-                for (var message in messages){
-                  final messageText = message.data()['text'];
-                  final messageSender = message.data()['user'];
-                  widgets.add(Text('$messageText from $messageSender'));
-                }
-                return Column(children: widgets,);
-              }
-              else {
-                return Center(
-                  child: CircularProgressIndicator(
-                    backgroundColor: Colors.lightBlueAccent,
-                  ),
-                );
-              }
-            },),
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -82,18 +67,16 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
-                      onChanged: (value) {
-                        message = value;
-                      },
+                      controller: controller,
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
                     onPressed: () {
-                      _firestore.collection('messages').add({
-                        'text': message,
-                        'user': loggedInUser.email
-                      });
+                      _firestore
+                          .collection('messages')
+                          .add({'text': controller.text, 'user': loggedInUser.email, 'timestamp': Timestamp.now()});
+                          controller.clear();
                     },
                     child: Text(
                       'Send',
@@ -106,6 +89,75 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({this.text, this.user, this.isMe});
+  final user;
+  final text;
+  final isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe?CrossAxisAlignment.end:CrossAxisAlignment.start,
+        children: [
+          Text(
+            user,
+            style: TextStyle(fontSize: 12.0, color: Colors.black54),
+          ),
+          Material(
+              borderRadius: isMe?BorderRadius.only(topLeft: Radius.circular(30.0), bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)):BorderRadius.only(topRight: Radius.circular(30.0), bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)),
+              elevation: 5.0,
+              color: (isMe)?Colors.lightBlueAccent: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Text(
+                  text,
+                  style: TextStyle(color: (isMe)?Colors.white:Colors.black54),
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  const MessageStream({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore.collection('messages').orderBy('timestamp').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final messages = snapshot.data!.docs.reversed;
+          List<MessageBubble> bubbles = [];
+          for (var message in messages) {
+            final messageText = message.data()['text'];
+            final messageSender = message.data()['user'];
+            bubbles.add(MessageBubble(text: messageText, user: messageSender, isMe: messageSender == loggedInUser.email));
+          }
+          return Expanded(
+              child: ListView(
+                reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: bubbles,
+          ));
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+      },
     );
   }
 }
