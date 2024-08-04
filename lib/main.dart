81e4272c996a1212ced202edfdc6df0388a34b31
Walid Tolba <wrt0624@gmail.com>
2024-08-04 13:56:18 +0100

    Add auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,45 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat_flutter/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/screens/welcome_screen.dart';
 import 'package:flash_chat_flutter/screens/login_screen.dart';
 import 'package:flash_chat_flutter/screens/registration_screen.dart';
 import 'package:flash_chat_flutter/screens/chat_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(FlashChat());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // FirestoreSerivce notes = FirestoreSerivce();
+    // notes.addNote('Nigga');
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        RegistrationScreen.id: (context) => RegistrationScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        ChatScreen.id: (context) => ChatScreen(),
-      }
-    );
+        debugShowCheckedModeBanner: false,
+        home: WelcomeScreen(),
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+        });
+  }
+}
+
+
+class FirestoreSerivce{
+  final CollectionReference notes = FirebaseFirestore.instance.collection('notes');
+  Future<void> addNote(String note){
+    return  notes.add({
+      'note': note,
+      'timestamp': Timestamp.now(),
+    });
   }
 }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_flutter/constants.dart';
 import 'package:flash_chat_flutter/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-    
+  final _auth = FirebaseAuth.instance;
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,24 +35,38 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
-              decoration: kInputDecoration.copyWith(hintText: 'Enter your email'),
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Enter your email'),
             ),
             SizedBox(
               height: 8.0,
             ),
             TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kInputDecoration.copyWith(hintText: 'Enter your password')
-            ),
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your password')),
             SizedBox(
               height: 24.0,
             ),
-            RoundedButton(text: 'Register', color: Colors.blueAccent, onPressed: (){})
+            RoundedButton(
+                text: 'Register', color: Colors.blueAccent, onPressed: ()async {
+                  try {
+                  final new_user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                  if (new_user != null)
+                  Navigator.pushNamed(context, 'chat_screen');
+                }catch(e) {
+                  print(e);
+                }
+                })
           ],
         ),
       ),
